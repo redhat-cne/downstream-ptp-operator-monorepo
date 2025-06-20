openshift/linuxptp-daemon

Add unit test for utils.GetAlias

pkg/utils/utils_test.go
@@ -1,0 +1,27 @@
+package utils_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/k8snetworkplumbingwg/linuxptp-daemon/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	ifname        string
+	expectedAlias string
+}
+
+func Test_GetAlias(t *testing.T) {
+	testCases := []testCase{
+		{"eth0", "ethx"},
+		{"eth1.100", "ethx.100"},
+		{"eth1.100.XYZ", "ethx.100.XYZ"},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%s->%s", tc.ifname, tc.expectedAlias), func(t *testing.T) {
+			assert.Equal(t, tc.expectedAlias, utils.GetAlias(tc.ifname))
+		})
+	}
+}

redhat-cne/downstream-ptp-operator-monorepo

pkg/linuxptp-daemon/pkg/utils/utils_test.go
@@ -1,0 +1,27 @@
+package utils_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/openshift/linuxptp-daemon/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	ifname        string
+	expectedAlias string
+}
+
+func Test_GetAlias(t *testing.T) {
+	testCases := []testCase{
+		{"eth0", "ethx"},
+		{"eth1.100", "ethx.100"},
+		{"eth1.100.XYZ", "ethx.100.XYZ"},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%s->%s", tc.ifname, tc.expectedAlias), func(t *testing.T) {
+			assert.Equal(t, tc.expectedAlias, utils.GetAlias(tc.ifname))
+		})
+	}
+}