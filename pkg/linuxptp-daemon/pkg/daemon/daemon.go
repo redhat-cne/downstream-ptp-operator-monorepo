openshift/linuxptp-daemon

GNR-D BC: Ignore e830 DPLL
This introduces a new PtpSettings value "dpll.$iface.ignore".  If set to
"true", the daemon will not initialize a dpll watcher for that
interface.  If unset, or overridden to any other value, the daemon
proceeds as normal.

Signed-off-by: Jim Ramsay <jramsay@redhat.com>

pkg/daemon/daemon.go
@@ -849,6 +849,10 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 				var eventSource []event.EventSource
 				if iface.Source == event.GNSS || iface.Source == event.PPS ||
 					(iface.Source == event.PTP4l && profileClockType == TBC) {
+					if nodeProfile.PtpSettings[dpll.PtpSettingsDpllIgnoreKey(iface.Name)] == "true" {
+						glog.Info("Init dpll: Skipping dpll for %s", iface.Name)
+						continue
+					}
 					glog.Info("Init dpll: ptp settings ", (*nodeProfile).PtpSettings)
 					for k, v := range (*nodeProfile).PtpSettings {
 						glog.Info("Init dpll: ptp kv ", k, " ", v)

redhat-cne/downstream-ptp-operator-monorepo

(cherry picked from commit 0ad3f23c2639698fe6a3a308781ef82db7c9e456)

pkg/linuxptp-daemon/pkg/daemon/daemon.go
@@ -762,6 +762,10 @@ func (dn *Daemon) applyNodePtpProfile(runID int, nodeProfile *ptpv1.PtpProfile) 
 				var eventSource []event.EventSource
 				if iface.Source == event.GNSS || iface.Source == event.PPS ||
 					(iface.Source == event.PTP4l && profileClockType == TBC) {
+					if nodeProfile.PtpSettings[dpll.PtpSettingsDpllIgnoreKey(iface.Name)] == "true" {
+						glog.Infof("Init dpll: Skipping dpll for %s", iface.Name)
+						continue
+					}
 					glog.Info("Init dpll: ptp settings ", (*nodeProfile).PtpSettings)
 					for k, v := range (*nodeProfile).PtpSettings {
 						glog.Info("Init dpll: ptp kv ", k, " ", v)