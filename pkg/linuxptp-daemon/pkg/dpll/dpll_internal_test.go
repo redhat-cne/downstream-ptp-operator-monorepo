openshift/linuxptp-daemon

Ignore phase offsets from non-input pins
Signed-off-by: Vitaly Grinberg <vgrinber@redhat.com>

pkg/dpll/dpll_internal_test.go
@@ -181,7 +181,7 @@ func TestActivePhaseOffsetPin(t *testing.T) {
 			pin: &nl.PinInfo{
 				ClockID: testClockID,
 				ParentDevice: []nl.PinParentDevice{
-					{ParentID: ppsDeviceID, State: nl.PinStateConnected},
+					{ParentID: ppsDeviceID, State: nl.PinStateConnected, Direction: nl.PinDirectionInput},
 				},
 			},
 			expectedIndex: 0,
@@ -233,8 +233,8 @@ func TestActivePhaseOffsetPin(t *testing.T) {
 			pin: &nl.PinInfo{
 				ClockID: testClockID,
 				ParentDevice: []nl.PinParentDevice{
-					{ParentID: eecDeviceID, State: nl.PinStateConnected},
-					{ParentID: ppsDeviceID, State: nl.PinStateConnected},
+					{ParentID: eecDeviceID, State: nl.PinStateConnected, Direction: nl.PinDirectionInput},
+					{ParentID: ppsDeviceID, State: nl.PinStateConnected, Direction: nl.PinDirectionInput},
 				},
 			},
 			expectedIndex: 1,

redhat-cne/downstream-ptp-operator-monorepo

pkg/linuxptp-daemon/pkg/dpll/dpll_internal_test.go
@@ -60,7 +60,7 @@ func TestActivePhaseOffsetPin(t *testing.T) {
 			pin: &nl.PinInfo{
 				ClockId: testClockID,
 				ParentDevice: []nl.PinParentDevice{
-					{ParentId: ppsDeviceID, State: nl.PinStateConnected},
+				{ParentId: ppsDeviceID, State: nl.PinStateConnected, Direction: nl.PinDirectionInput},
 				},
 			},
 			expectedIndex: 0,
@@ -112,8 +112,8 @@ func TestActivePhaseOffsetPin(t *testing.T) {
 			pin: &nl.PinInfo{
 				ClockId: testClockID,
 				ParentDevice: []nl.PinParentDevice{
-					{ParentId: eecDeviceID, State: nl.PinStateConnected},
-					{ParentId: ppsDeviceID, State: nl.PinStateConnected},
+				{ParentId: eecDeviceID, State: nl.PinStateConnected, Direction: nl.PinDirectionInput},
+				{ParentId: ppsDeviceID, State: nl.PinStateConnected, Direction: nl.PinDirectionInput},
 				},
 			},
 			expectedIndex: 1,