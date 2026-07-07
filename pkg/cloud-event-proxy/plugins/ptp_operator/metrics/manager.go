redhat-cne/cloud-event-proxy

OCPBUGS-62812: Propogate FREERUN for CLOCK_REALTIME

plugins/ptp_operator/metrics/manager.go
@@ -40,6 +40,7 @@ type PTPEventManager struct {
 	// Ptp4lConfigInterfaces holds interfaces and its roles, after reading from ptp4l config files
 	Ptp4lConfigInterfaces map[types.ConfigName]*ptp4lconf.PTP4lConfig
 	lastOverallSyncState  ptp.SyncState
+	lastOverallGnssState  ptp.SyncState
 	processStates         map[types.ConfigName]map[string]bool
 }
 

redhat-cne/downstream-ptp-operator-monorepo

pkg/cloud-event-proxy/plugins/ptp_operator/metrics/manager.go
@@ -38,6 +38,7 @@ type PTPEventManager struct {
 	// Ptp4lConfigInterfaces holds interfaces and its roles, after reading from ptp4l config files
 	Ptp4lConfigInterfaces map[types.ConfigName]*ptp4lconf.PTP4lConfig
 	lastOverallSyncState  ptp.SyncState
+	lastOverallGMState    ptp.SyncState
 	processStates         map[types.ConfigName]map[string]bool
 }
 